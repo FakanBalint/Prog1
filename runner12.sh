b93f7c88614c56a3f395d671ddb1c1179f070320
FakanBalint <fakan.balint@gmail.com>
2022-02-23 17:05:26 +0100

    drill 13 kész

---
+++ runner12.sh
@@ -0,0 +1,1 @@
+g++ drill12.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill12 `fltk-config --ldflags --use-images` -std=c++11 
---
+++ runner13.sh
@@ -0,0 +1,1 @@
+g++ drill13.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill13 `fltk-config --ldflags --use-images` -std=c++11 
